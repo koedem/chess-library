a2e886bbf4872c7dcbd9285d689d0eb5ffb4d13d
Kolja Kühn <kolja@heck-kuehn.de>
2023-02-03 22:58:20 +0100

    Add the methods I need into the original Board class.

--- src/chess.hpp
+++ src/chess.hpp
@@ -1045,6 +1045,24 @@ class Board
 
     void removeCastlingRightsAll(Color c);
     void removeCastlingRightsRook(Square sq);
+
+    // Fork additions:
+    enum Eval_Type {
+        Incremental_PST,
+        Full_PST,
+        Random,
+        Pseudo_random
+    };
+
+    constexpr static Eval_Type EVAL_TYPE = Incremental_PST;
+
+    int midgame_PST = 0, endgame_PST = 0, game_phase = 0;
+
+public:
+    template<Eval_Type type>
+    int eval();
+
+    int eval();
 };
 
 inline Board::Board(std::string fen)
